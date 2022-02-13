b6a8538910e0371f9ab52ea84848c82394e395d8
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2022-02-13 07:47:01 +0100

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      accounts: [''],
+    },
+  },
+};
